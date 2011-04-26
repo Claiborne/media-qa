6f764ed1d5ccbd4e7c83694e750fa5557d6e87ff
Frank Klun <fklun@ign.com>
2011-04-26 16:06:51 -0700

    initial upload of paperclip application

---
+++ paperclip/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PaperclipV2::Application.config.session_store :cookie_store, :key => '_Paperclip_v2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PaperclipV2::Application.config.session_store :active_record_store
