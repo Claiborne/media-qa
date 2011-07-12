11b351adb44737539e6014e7814db45cfa49f735
Sagy Wiessbrod <sagy@CMMBPSWEISSBROD.local>
2011-07-12 16:39:21 -0700

    adding first v1 test

---
+++ test/lib/configuration.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+class Configuration
+  attr_accessor :options
+
+  def self.config_path=(path)
+    @@config_path = path
+  end
+
+  def initialize
+    raise ConfigurationException, "Missing configuration file" unless File.exists?(@@config_path)
+    environment = ENV['env'] 
+    configs = YAML.load_file(@@config_path)
+    @options = configs[environment]    
+  end
+end
+
+class ConfigurationException < StandardError; end;
