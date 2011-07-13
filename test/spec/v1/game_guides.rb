784623bd4fc86ddd9ffd089883887cd924b3cede
Sagy Wiessbrod <sagy@cmmbpsweissbrod.home>
2011-07-13 12:30:54 -0700

    adding tests for games resource

---
+++ test/spec/v1/game_guides.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+require 'rest_client'
+require 'json'
+require 'configuration'
+
+describe "game guides" do
+
+  before(:all) do
+
+  end
+
+  before(:each) do
+    Configuration.config_path = File.dirname(__FILE__) + "/../../config/v1.yml"
+    @config = Configuration.new
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return valid game guides" do
+   response = RestClient.get "http://#{@config.options['baseurl']}/v1/games/ /guides"    
+   response.code.should eql(200)
+   data = JSON.parse(response.body)   
+  end
+
+end
