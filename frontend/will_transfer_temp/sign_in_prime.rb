83d18b77770a7d25700d292fd702b4123247d044
Will Claiborne <wclaiborne@ign.com>
2011-07-29 12:28:05 -0700

    removed temp transfer files

--- frontend/will_transfer_temp/sign_in_prime.rb
+++
@@ -1,6 +0,0 @@
-	@selenium.open "http://my.ign.com/login?r=http://www.ign.com/"
-    @selenium.click "emailField"
-    @selenium.type "emailField", "smoketest@testign.com"
-    @selenium.type "passwordField", "testpassword"
-    @selenium.click "signinButton"
-    @selenium.wait_for_page_to_load "40"
