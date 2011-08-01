b3bd678010a4d96a8f9c11c1b9f345a88629fdee
Mediaqa <mediaqa-git@ign.com>
2011-08-01 14:28:31 -0700

    updated login_page, added provider_login_page, and config

--- test/lib/social/login_page.rb
+++ test/lib/social/login_page.rb
@@ -1,16 +1,20 @@
+require 'page'
+require 'providers'
+
 module Oyster
  module Social
   class LoginPage < Page
  
-    def visit(url="http://#{@config.options['baseurl}/login")
+  def visit(url="http://#{@config.options['baseurl_myign']}/login")
      @client.open(url)
+     @client.wait_for_page_to_load
     end
 
     def login(user, password)
      @client.type 'emailField', user
      @client.type 'passwordField', password
-     @client.click 'SIGN IN'
-     @client.wait
+     @client.click "//button[@class='submit']"
+     @client.wait_for_page_to_load
     end
 
     def signup
@@ -19,8 +23,16 @@ module Oyster
     end  
 
     def login_openid(provider)
-      @client.click "/button[@class='#{provider}']"
-      @client.wait
+      @client.click "//button[@class='#{provider}']"
+      @client.wait_for_page_to_load
+      provider_page = ProviderLoginPage.new @client, @config, provider
+      provider_page
+    end
+
+    def assert_invalid_authentication_message
+      expected_msg = 'You cannot login using the current credentials. Please try again.'
+      @client.get_text('//span[@class="errorMsg"]').should == expected_msg
+
     end
   end
  end
