fee26863e6c0c7e3b6b0f4c7d3908b5520b6608e
Mediaqa <mediaqa-git@ign.com>
2011-08-03 11:16:03 -0700

    adding social files

---
+++ test/lib/social/my_profile_page.rb
@@ -0,0 +1,48 @@
+require 'page'
+
+module Oyster
+ module Social
+  class MyProfilePage < Page
+
+    def initialize(client,config)
+      super(client,config)
+    end
+
+    def is_following?
+      flag = true
+      
+      attribute_str = @client.get_attribute("css=div.socialProfileHeader div.addToIGN@style")
+      if attribute_str.include?("follow_")
+        flag = false
+      end
+      flag
+    end
+
+    def start_following_person
+      # TODO add is_element_present logic
+      if !self.is_following?
+        @client.click "css=div.socialProfileHeader div.profileInfo div[id^='myIgnFollowPerson'] div.addToIGNContainer div.addToIGN"
+      end
+    end
+
+    def stop_following_person
+      # TODO add is_element_present logic
+      if self.is_following?
+        @client.click "css=div.socialProfileHeader div.profileInfo div[id^='myIgnFollowPerson'] div.addToIGNContainer div.addToIGN"
+        @client.click "css=div.socialProfileHeader div[id^='myIgnFollowPerson'] div.removeFromIGN"
+      end
+    end
+
+    def is_wall_post_entry_available?
+      flag = @client.is_element_present("css=div#updateStatusContainer input#statusField")
+      flag
+    end
+
+    def submit_wall_post
+      if self.is_wall_post_entry_available?
+        #TODO enter text and click add button
+      end
+    end
+  end
+ end
+end
