6bbe9b6ceb35345bb944fe2717501cdec7568384
Sagy Wiessbrod <sagy@cmmbpsweissbrod.home>
2011-09-01 14:28:34 -0700

    add cases for token route

--- test/spec/oauth2/oauth.rb
+++ test/spec/oauth2/oauth.rb
@@ -24,30 +24,117 @@ describe "oauth2 token" do
 
   end
 
-  it "should return valid articles" do
-   response = RestClient.post "http://#{@config.options['baseurl']}/ent/entitlement?name=posts&description=test", {}
+  it "should return return token" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
    response.code.should eql(200)  
  
-   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlements=posts", {}
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
    response.code.should eql(200)
    data = JSON.parse(response.body)
    client_id = data['client_id']
    client_secret = data['client_secret']
 
-   response = RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_id=QA&client_secret=#{client_secret}", {} 
+   response = RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_id=#{client_id}&client_secret=#{client_secret}", {} 
    response.code.should eql(200)  
    data = JSON.parse(response.body)
-   access_token = data['access_token']
-
-   response = RestClient.get "http://#{@config.options['baseurl']}/admin/valid?access_token=#{access_token}"
-   response.code.should eql(200)
-
-   response = RestClient.delete "http://#{@config.options['baseurl']}/admin/revoke?client_id=QA&client_secret=#{client_secret}&access_token=#{access_token}"
-   response.code.should eql(200)
- 
-   response = RestClient.get  "http://#{@config.options['baseurl']}/admin/valid?access_token=#{access_token}"
-   response.code.should eql(400)
+   data['access_token'].should_not be_nil
   
   end
 
+    it "should return not return token with invalid type" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
+   response.code.should eql(200) 
+
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
+   response.code.should eql(200)
+   data = JSON.parse(response.body)
+   client_id = data['client_id']
+   client_secret = data['client_secret']
+
+    lambda {
+      RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=wrong_type&client_id=#{client_id}&client_secret=#{client_secret}", {}
+    }.should raise_error(Exception) { |error|
+      error.response.code.should eql(400)
+      data = JSON.parse(error.response.body)
+      data['error'].should eql('unsupported_grant_type')
+    }
+
+  end
+
+   it "should return not return token with invalid client id" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
+   response.code.should eql(200)
+
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
+   response.code.should eql(200)
+   data = JSON.parse(response.body)
+   client_id = data['client_id']
+   client_secret = data['client_secret']
+
+  lambda {
+      RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_id=blah&client_secret=#{client_secret}", {}
+    }.should raise_error(Exception) { |error|
+      error.response.code.should eql(400)
+      data = JSON.parse(error.response.body)
+      data['error'].should eql('unauthorized_client')
+    }
+  end
+
+  
+   it "should return not return token with invalid secret" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
+   response.code.should eql(200)
+
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
+   response.code.should eql(200)
+   data = JSON.parse(response.body)
+   client_id = data['client_id']
+   client_secret = data['client_secret']
+
+  lambda {
+      RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_id=#{client_id}&client_secret=blah", {}
+    }.should raise_error(Exception) { |error|
+      error.response.code.should eql(400)
+      data = JSON.parse(error.response.body)
+      data['error'].should eql('unauthorized_client')
+    }
+  end
+
+    it "should return not return token with missing client parameter" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
+   response.code.should eql(200)
+
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
+   response.code.should eql(200)
+   data = JSON.parse(response.body)
+   client_id = data['client_id']
+   client_secret = data['client_secret']
+
+  lambda {
+      RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_secret=#{client_secret}", {}
+    }.should raise_error(Exception) { |error|
+      error.response.code.should eql(400)
+      data = JSON.parse(error.response.body)
+      data['error'].should eql('invalid_request')
+    }
+  end
+
+    it "should return not return token with missing secret parameter" do
+   response = RestClient.post "http://#{@config.options['baseurl']}/entitlement", { name: 'posts', description: 'test'}
+   response.code.should eql(200)
+
+   response = RestClient.post "http://#{@config.options['baseurl']}/admin/register?name=QA&description=test&entitlement=posts", {}
+   response.code.should eql(200)
+   data = JSON.parse(response.body)
+   client_id = data['client_id']
+   client_secret = data['client_secret']
+
+  lambda {
+      RestClient.post "http://#{@config.options['baseurl']}/token?grant_type=client_credentials&client_id=#{client_id}", {}
+    }.should raise_error(Exception) { |error|
+      error.response.code.should eql(400)
+      data = JSON.parse(error.response.body)
+      data['error'].should eql('invalid_request')
+    }
+  end
 end
