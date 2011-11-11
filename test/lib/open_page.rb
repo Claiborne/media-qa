381fbb78be40c20150e6c9b9b32af78cafa5a9ab
Will Claiborne <wclaiborne@ign.com>
2011-11-10 16:35:17 -0800

    debug code added

--- test/lib/open_page.rb
+++ test/lib/open_page.rb
@@ -17,13 +17,16 @@ end#end def
 
 def rest_client_open(page)
   stitial_count = 0
+  puts "opening page normally"+stitial_count.to_s
   rest_doc = RestClient.get(page)
   while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
+    puts "opening page after stitial"+stitial_count.to_s
     rest_doc = RestClient.get(page)
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
     elsif stitial_count > 2
+      puts "opening page with special=noads"+stitial_count.to_s
       rest_doc = RestClient.get(page+"?special=noads")
     end
   end#end while
