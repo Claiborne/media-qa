e57893ee51fbde3d174a88ab5e48cb8597120f0a
Will Claiborne <wclaiborne@ign.com>
2011-11-15 12:59:01 -0800

    rewrote RestClient::Exception to provide more useful information

--- test/lib/open_page.rb
+++ test/lib/open_page.rb
@@ -3,8 +3,10 @@ module OpenPage
 def nokogiri_open(page)
   stitial_count = 0
   nok_doc = Nokogiri::HTML(open(page))
+  sleep 1
   while nok_doc.at_css('div#disable')
     nok_doc = Nokogiri::HTML(open(page))
+    sleep 1
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
@@ -16,34 +18,27 @@ def nokogiri_open(page)
 end#end def
 
 def rest_client_open(page)
-  puts page ########
   stitial_count = 0
-  ############
   begin
-  rest_doc = RestClient.get(page)
-  rescue => e
-  raise Exception.new("#{e.http_code} Error on page without encountering a stitial: "+page.to_s)
-  end
-  ############
-  while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
-    ############
-    begin
     rest_doc = RestClient.get(page)
+  rescue => e
+    raise Exception.new("#{e.http_code} error on: "+page.to_s)
+  end#end Exception
+  while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
+    begin
+      rest_doc = RestClient.get(page)
     rescue => e
-    raise Exception.new("#{e.http_code} Error on page WITH encountering a stitial: "+page.to_s)
-    end
-    ############
+      raise Exception.new("#{e.http_code} error on (after skipping a stitial): "+page.to_s)
+    end#end Exception
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
     elsif stitial_count > 2
-      ############
       begin
-      rest_doc = RestClient.get(page+"?special=noads")
-    rescue => e
-    raise Exception.new("#{e.http_code} Error on page with special=stital: "+page.to_s)
-      end
-      ############
+        rest_doc = RestClient.get(page+"?special=noads")
+      rescue => e
+        raise Exception.new("#{e.http_code} error on page (with special=stital): "+page.to_s)
+      end#end Exception
     end
   end#end while
   return rest_doc
