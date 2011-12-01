acc7e59be2231bddf4209df81fbb3a95a85f3170
Will Claiborne <wclaiborne@ign.com>
2011-12-01 13:44:41 -0800

    removed possible naming conflicts

--- test/lib/open_page.rb
+++ test/lib/open_page.rb
@@ -1,107 +1,107 @@
 module OpenPage
 
-def nokogiri_open(page)
+def nokogiri_open(n_page)
   stitial_count = 0 
   begin
-    nok_doc = Nokogiri::HTML(open(page))
+    nok_doc = Nokogiri::HTML(open(n_page))
   rescue => e
-    raise Exception.new("#{e.message} on "+page.to_s )
+    raise Exception.new("#{e.message} on "+n_page.to_s )
   end#end Exception
   while nok_doc.at_css('div#disable')
     begin
-      nok_doc = Nokogiri::HTML(open(page))
+      nok_doc = Nokogiri::HTML(open(n_page))
     rescue => e
-      raise Exception.new("#{e.message} (after skipping a stitial) on "+page.to_s )
+      raise Exception.new("#{e.message} (after skipping a stitial) on "+n_page.to_s )
     end#end Exception
     stitial_count +=1
     if stitial_count > 3
-      raise Exception.new("An endless stitial loop on #{page} prevented this test case from running")
+      raise Exception.new("An endless stitial loop on #{n_page} prevented this test case from running")
     elsif stitial_count > 2
       begin
-        nok_doc = Nokogiri::HTML(open(page+"?special=noads"))
+        nok_doc = Nokogiri::HTML(open(n_page+"?special=noads"))
       rescue => e
-        raise Exception.new("#{e.message} (with special=stital) on "+page.to_s)
+        raise Exception.new("#{e.message} (with special=stital) on "+n_page.to_s)
       end#end Exception
     end
   end#end while
   return nok_doc
 end#end def
 
-def rest_client_open(page)
+def rest_client_open(r_page)
   stitial_count = 0
   begin
-    rest_doc = RestClient.get(page)
+    rest_doc = RestClient.get(r_page)
   rescue => e
-    raise Exception.new("#{e.message} on "+page.to_s)
+    raise Exception.new("#{e.message} on "+r_page.to_s)
   end#end Exception
   while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
     begin
-      rest_doc = RestClient.get(page)
+      rest_doc = RestClient.get(r_page)
     rescue => e
-      raise Exception.new("#{e.message} (after skipping a stitial) on "+page.to_s)
+      raise Exception.new("#{e.message} (after skipping a stitial) on "+r_page.to_s)
     end#end Exception
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
     elsif stitial_count > 2
       begin
-        rest_doc = RestClient.get(page+"?special=noads")
+        rest_doc = RestClient.get(r_page+"?special=noads")
       rescue => e
-        raise Exception.new("#{e.message} (with special=stital) on "+page.to_s)
+        raise Exception.new("#{e.message} (with special=stital) on "+r_page.to_s)
       end#end Exception
     end
   end#end while
   return rest_doc
 end
 
-def rest_client_not_301_home_open(page)
+def rest_client_not_301_home_open(r2_page)
   stitial_count = 0
   begin
-    rest_doc = rest_client_not_301_home(page)
+    rest_doc = rest_client_not_301_home(r2_page)
   rescue => e
-    raise Exception.new("#{e.message} on "+page.to_s)
+    raise Exception.new("#{e.message} on "+r2_page.to_s)
   end#end Exception
   while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
     begin
-      rest_doc = rest_client_not_301_home(page)
+      rest_doc = rest_client_not_301_home(r2_page)
     rescue => e
-      raise Exception.new("#{e.message} (after skipping a stitial) on "+page.to_s)
+      raise Exception.new("#{e.message} (after skipping a stitial) on "+r2_page.to_s)
     end#end Exception
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
     elsif stitial_count > 2
       begin
-        rest_doc = rest_client_not_301_home(page+"?special=noads")
+        rest_doc = rest_client_not_301_home(r2_page+"?special=noads")
       rescue => e
-        raise Exception.new("#{e.message} (with special=stital) on "+page.to_s)
+        raise Exception.new("#{e.message} (with special=stital) on "+r2_page.to_s)
       end#end Exception
     end
   end#end while
   return rest_doc
 end
 
-def rest_client_not_310_open(page)
+def rest_client_not_310_open(r3_page)
   stitial_count = 0
   begin
-    rest_doc = rest_client_not_310(page)
+    rest_doc = rest_client_not_310(r3_page)
   rescue => e
-    raise Exception.new("#{e.message} on "+page.to_s)
+    raise Exception.new("#{e.message} on "+r3_page.to_s)
   end#end Exception
   while Nokogiri::HTML(rest_doc.body).at_css('div#disable')
     begin
-      rest_doc = rest_client_not_310(page)
+      rest_doc = rest_client_not_310(r3_page)
     rescue => e
-      raise Exception.new("#{e.message} (after skipping a stitial) on "+page.to_s)
+      raise Exception.new("#{e.message} (after skipping a stitial) on "+r3_page.to_s)
     end#end Exception
     stitial_count +=1
     if stitial_count > 3
       raise "An endless stitial loop prevented this test case from running"
     elsif stitial_count > 2
       begin
-        rest_doc = rest_client_not_310(page+"?special=noads")
+        rest_doc = rest_client_not_310(r3_page+"?special=noads")
       rescue => e
-        raise Exception.new("#{e.message} (with special=stital) on "+page.to_s)
+        raise Exception.new("#{e.message} (with special=stital) on "+r3_page.to_s)
       end#end Exception
     end
   end#end while
@@ -109,11 +109,11 @@ def rest_client_not_310_open(page)
 end
 
 
-def rest_client_not_301_home(page)
-  RestClient.get(page){ |response, request, result, &block|
+def rest_client_not_301_home(r4_page)
+  RestClient.get(r4_page){ |response, request, result, &block|
     if response.code == 301
       if ["http://www.ign.com", "http://www.ign.com/"].include? response.headers[:location]
-        raise Exception.new("#{page} did not return a 200 but instead a #{response.code} to #{response.headers[:location]}")
+        raise Exception.new("#{r4_page} did not return a 200 but instead a #{response.code} to #{response.headers[:location]}")
       else
         response.return!(request, result, &block)
       end
@@ -122,13 +122,13 @@ def rest_client_not_301_home(page)
     end }
 end
 
-def rest_client_not_310(page)
-  RestClient.get(page){ |response, request, result, &block|
+def rest_client_not_310(r5_page)
+  RestClient.get(r5_page){ |response, request, result, &block|
     if response.code == 301
       if [/4\d\d/, /5\d\d/].include? response.follow_redirection(request, result, &block).code
         response.follow_redirection(request, result, &block)
       else
-        raise Exception.new("#{page} did not return a 200 but instead a #{response.code} to #{response.headers[:location]}")
+        raise Exception.new("#{r5_page} did not return a 200 but instead a #{response.code} to #{response.headers[:location]}")
       end
     else
       response.return!(request, result, &block)
