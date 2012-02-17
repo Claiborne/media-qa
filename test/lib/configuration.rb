b0804f768cacd1e27c4718bda5cd7e065ae2d704
Will Claiborne <wclaiborne@ign.com>
2012-02-16 21:45:19 -0800

    images fe automation

--- test/lib/configuration.rb
+++ test/lib/configuration.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 class Configuration
   attr_accessor :options
+  attr_accessor :browser #
 
   def self.config_path=(path)
     @@config_path = path
@@ -9,9 +10,11 @@ class Configuration
 
   def initialize
     raise ConfigurationException, "Missing configuration file" unless File.exists?(@@config_path)
-    environment = ENV['env'] 
+    environment = ENV['env']
+    browser = ENV['browser'] #
     configs = YAML.load_file(@@config_path)
     @options = configs[environment]
+    @browser = configs[browser] #
   
     # this is a bad hack for branch substitution 
     @options['baseurl'].sub(/branchname/, ENV['branch']) unless ENV['branch'] == nil    
--- test/spec/oyster/media/images.rb
+++ test/spec/oyster/media/images.rb
@@ -1,19 +1,30 @@
 require 'rspec'
+require 'selenium-webdriver'
 require 'configuration'
-require 'nokogiri'
-require 'rest_client'
-require 'open_page'
-require 'fe_checker'
-
-include OpenPage
+require 'rest-client'
+=begin
+#
+#
+#
+#      IMPLEMENT BROWSER AND ENV
+#
+#
+#
+#
 
 describe "Images HomePage:" do
 
   before(:all) do
-    Configuration.config_path = File.dirname(__FILE__) + "/../../../config/tech.yml"########change this
+    Configuration.config_path = File.dirname(__FILE__) + "/../../../config/oyster/oyster_media.yml"
     @config = Configuration.new
     @page = "http://#{@config.options['baseurl']}/images"
     puts @page
+    @selenium = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+  end
+  
+  after(:all) do
+    @selenium.quit
   end
 
   before(:each) do
@@ -24,21 +35,29 @@ describe "Images HomePage:" do
 
   end
   
-  it "should  301 to /index/images.html", :smoke => true do
-    RestClient.get(@page) { |response, request, result, &block|
-    ["http://www.ign.com/index/images.html","http://www.ign.com/index/images.html/"].include?(response.headers[:location].to_s).should be_true }
+  it "should 301 from /images to /index/images.html", :smoke => true do
+    @selenium.get @page
+    #
+    # Wait for page somehow
+    #
+    @selenium.current_url.should == "http://#{@config.options['baseurl']}/index/images.html"
   end
   
 end
-
-describe "Images Gallery:" do
+=end
+describe "Images Gallery Page:", :selenium => true do
 
   before(:all) do
-    Configuration.config_path = File.dirname(__FILE__) + "/../../../config/tech.yml"########change this
+    Configuration.config_path = File.dirname(__FILE__) + "/../../../config/oyster/oyster_media.yml"
     @config = Configuration.new
-    @page = "http://#{@config.options['baseurl']}/images/games/metal-gear-solid-4-guns-of-the-patriots-ps3-714044"
+    @page = "http://#{@config.options['baseurl']}/images/games/far-cry-3-xbox-360-53491"
     puts @page
-    @doc = nokogiri_not_301_open(@page)
+    @selenium = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+  end
+  
+  after(:all) do
+    @selenium.quit
   end
 
   before(:each) do
@@ -49,26 +68,78 @@ describe "Images Gallery:" do
 
   end
   
-  it "should return 200", :smoke => true do
+  it "should open the Far Cry 3 gallery page", :smoke => true do
+    @selenium.get @page
+    #
+    # Wait for page somehow
+    #
+    @selenium.current_url.should == @page
+  end
+
+  it "should display 20 thumbnails", :smoke => true do
+    @selenium.find_elements(:css => "div.imageGallery div.imageGalleryThumb a img[src*='http']").size.should == 20
   end
   
-  it "should contain header text 'Metal Gear Solid 4 Images'" do
-    @doc.css('h1.contentHeader').text.should == 'Metal Gear Solid 4 Images'
-  end
-  
-  it "should display 20 images", :smoke => true do
-    @doc.css('div.imageGalleryThumb img').count.should == 20
-    @doc.css('div.imageGalleryThumb img').each do |img|
-      img.attribute('src').to_s.match(/http/).should be_true
+  it "should not display any broken thumbnails", :spam => true do
+    @selenium.find_elements(:css => "div.imageGallery div.imageGalleryThumb a img").each do |i|
+      RestClient.get(i.attribute('src')).code.should == 200
     end
   end
   
-  it "should "
+  it "should display pagination", :smoke => true do
+    @selenium.find_element(:css => "div.pagination span")
+    @selenium.find_element(:css => "div.pagination a")
+  end
   
-  it "should contain pagination", :smoke => true do
+  it "should display pagination with a link to the second page" do
+   @selenium.find_element(:css => "div.pagination a").attribute('href').match(/\?page=2/)
+  end
+  
+  it "should display a non-broken image above the thumbnails when a thumbnail is clicked ", :smoke => true do
+    page_first_thumb = @selenium.find_element(:css => "div.imageGallery div.imageGalleryThumb a")
+    page_first_thumb.click
+    page_image = @wait.until { @selenium.find_element(:css => "div#peekWindow a img[src*='http']") }
+    RestClient.get(page_image.attribute('src')).code.should == 200
+  end
+  
+  it "should change the URL when a thumbnail is cliked", :smoke => true do
+    @selenium.current_url.match(/far-cry-3-xbox-360-53491\/\d\d\d/).should be_true
+  end
+  
+  it "should display the appropriate images and URLs when the back and forward browser buttons are clicked", :smoke => true do
+    # Set variables for the starting image and starting UR:
+    starting_image =  @selenium.find_element(:css => "div#peekWindow a img").attribute('src')
+    starting_url = @selenium.current_url
+    
+    # Click the second thumbnail image and wait until it replaces the starting image in the viewer
+    @selenium.find_element(:css => "div.imageGallery div:nth-child(2).imageGalleryThumb a").click
+    next_image = @selenium.find_element(:css => "div#peekWindow a img")
+    @wait.until { next_image.attribute('src') != starting_image }
+    
+    # Wait for the URL to change
+    @wait.until { @selenium.current_url != starting_url }
+    
+    # Press the back button on the browser
+    @selenium.navigate.back
+    
+    # Wait for the starting image to replace the previous image
+    @wait.until { @selenium.find_element(:css => "div#peekWindow a img").attribute('src') == starting_image }
+    
+    # Wait for the URL to change to the starting URL
+    @wait.until { @selenium.current_url == starting_url }
+      
+    # Check clicking back from here takes you to the original gallery page with no image in the viewr
+    @selenium.navigate.back
+    @wait.until { @selenium.find_element(:css => "div#peekWindow a img").displayed? == false }
+    @wait.until { @selenium.current_url == @page }
+    
+    # Check clicking forward from here takes you back to the previous image you were viewing
+    @selenium.navigate.forward
+    @wait.until { @selenium.find_element(:css => "div#peekWindow a img").attribute('src') == starting_image }
+    @wait.until { @selenium.current_url == starting_url }
     
   end
   
+
+  
 end
-
-
