03abaeb374154b48188b532b255e9fcb104345d3
Will Claiborne <wclaiborne@ign.com>
2012-07-30 16:08:01 -0700

    better link checker app

---
+++ test/spec/link_checker/link_checker.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+require 'rest_client'
+
+# EXAMPLE HOW TO RUN
+
+# rake checklinks css='div div.topgames-module' url='www.ign.com/games/reviews/xbox-360'
+
+puts ""
+puts "BROKEN LINKS:"
+
+@doc = Nokogiri::HTML(RestClient.get(ENV['url']))
+
+@doc.css("#{ENV['css']} a").each do |a|
+
+  link = a.attribute('href').to_s
+
+  begin
+    RestClient.get link
+  rescue => e
+    puts link.to_s
+  end
+
+end
+
+puts ""
+
+
