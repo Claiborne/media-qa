8ef9d4f73c0a6500ecd088d520ab96d6cfa10742
Will Claiborne <wclaiborne@ign.com>
2012-08-22 11:36:42 -0700

    slight refractor/renaming object page spec files

---
+++ test/spec/oyster/media/comic_object_page.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'configuration'
+require 'nokogiri'
+require 'rest_client'
+require 'open_page'
+require 'fe_checker'
+require 'json'
+
+include OpenPage
+include FeChecker
---
+++ test/spec/v3/object/create_all_fields.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+require 'nokogiri'
+require 'configuration'
+require 'rest_client'
+require 'json'
+require 'assert'
+require 'topaz_token'
+
+include Assert
+include TopazToken
+
+=begin
+
+describe "V3 Object API -- Create A Release With All Fields", :test => true do
+
+  before(:all) do
+    @token = return_topaz_token('objects')
+    Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_object.yml"
+    @config = Configuration.new
+    @url = "http://media-object-stg-services-01.sfdev.colo.ignops.com:8080/object/v3/releases?oauth_token=#{CreateSmokeHelperVars.return_token}"
+    begin
+      @response = RestClient.post @url, V3ObjCreateSmoke.create_valid_release, :content_type => "application/json"
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    puts @data
+  end
+
+end
+
+=end
