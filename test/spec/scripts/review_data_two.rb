b3185fef0b221ac7d7a3526c4a5f3d87f39a2a6f
Will Claiborne <wclaiborne@ign.com>
2012-09-25 15:17:54 -0700

    changing way I check for entertainment data

--- test/spec/scripts/review_data_two.rb
+++ test/spec/scripts/review_data_two.rb
@@ -1,18 +1,21 @@
-require 'oci8'
-require 'dbi'
+#require 'dbi'
+#require 'oci8'
 require 'rspec'
 require 'nokogiri'
 require 'configuration'
 require 'rest_client'
 require 'json'
 
-dev_connect = "DBI:OCI8:media-dev-nib-db-01.sfdev.colo.ignops.com:1521/nibdb"
-dev_user = "publish"
-dev_pass = "publishdev"
+#dev_connect = "DBI:OCI8:media-dev-nib-db-01.sfdev.colo.ignops.com:1521/nibdb"
+#dev_user = "publish"
+#dev_pass = "publishdev"
 
-connect = "DBI:OCI8:nibdb4.las1.colo.ignops.com:1521/nibdb"
-user = "publish"
-pass = "saturn1"
+#connect = "DBI:OCI8:nibdb4.las1.colo.ignops.com:1521/nibdb"
+#user = "publish"
+#pass = "saturn1"
+
+wordpress_review_urls =[]
+wordpress_preview_urls = []
 
 def ent_review_articles
   {"matchRule"=>"matchAll",
@@ -58,10 +61,8 @@ def ent_preview_articles
   }.to_json
 end
 
-begin
-
   #db = DBI.connect(dev_connect,dev_user,dev_pass)
-  db = DBI.connect(connect,user,pass)
+  #db = DBI.connect(connect,user,pass)
 
   puts ""
   puts "--------------------- ENT REVIEWS ---------------------"
@@ -79,9 +80,8 @@ begin
     else
       objectRelations = []
     end
-
+=begin
     objectRelations.each do |obj|
-      sleep 0.2
       x = db.execute("SELECT review_url, overall_rating FROM obj_network_resources WHERE obj_id = '#{obj}' and network = '12'")
 
       x.fetch_hash do |row|
@@ -91,7 +91,7 @@ begin
         end
       end
     end
-
+=end
     # CHECK V3 REVIEW DATA EXISTS
 
     catch (:error_404) do
@@ -108,17 +108,20 @@ begin
             begin
               game_data['network']['ign']['review']['score']
             rescue
+              wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
               puts object
               next
             end
             begin
               game_data['legacyData']['reviewUrl']
             rescue
+              wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
               puts object
               next
             end
             if (game_data['network']['ign']['review']['score'].to_s.length < 1 || game_data['legacyData']['reviewUrl'].to_s.match(/com\/articles\//) || game_data['legacyData']['reviewUrl'].to_s.match(/blogs/) || game_data['legacyData']['reviewUrl'].to_s.length < 1 || game_data['network']['ign']['review']['score'] == nil || game_data['legacyData']['reviewUrl'] == nil)
             else
+              wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
               puts object
             end
           end
@@ -138,17 +141,20 @@ begin
         begin
           game_data['network']['ign']['review']['score']
         rescue
+          wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
           puts object
           next
         end
         begin
           game_data['legacyData']['reviewUrl']
         rescue
+          wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
           puts object
           next
         end
         if (game_data['network']['ign']['review']['score'].to_s.length < 1 || game_data['legacyData']['reviewUrl'].to_s.match(/com\/articles\//) || game_data['legacyData']['reviewUrl'].to_s.match(/blogs/) || game_data['legacyData']['reviewUrl'].to_s.length < 1 || game_data['network']['ign']['review']['score'] == nil || game_data['legacyData']['reviewUrl'] == nil)
         else
+          wordpress_review_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
           puts object
         end
       end
@@ -156,6 +162,7 @@ begin
     end
   end
 
+  puts wordpress_review_urls
   # END REVIEWS
 
   puts ""
@@ -167,7 +174,6 @@ begin
   response = RestClient.get url
   data = JSON.parse(response.body)
   data['data'].each do |article|
-
     if article.has_key?('legacyData')
       if article["legacyData"].has_key?('objectRelations')
         objectRelations = article["legacyData"]["objectRelations"]
@@ -175,9 +181,8 @@ begin
     else
       objectRelations = []
     end
-
+=begin
     objectRelations.each do |obj|
-      sleep 0.5
       x = db.execute("SELECT preview_url FROM obj_network_resources WHERE obj_id = '#{obj}' and network = '12'")
 
       x.fetch_hash do |row|
@@ -187,62 +192,53 @@ begin
         end
       end
     end
-
+=end
     # CHECK V3 PREVIEW DATA EXISTS
-    catch (:error_404) do
+
       objectRelations.each do |object|
         begin
           object_response = RestClient.get "http://apis.lan.ign.com/object/v3/releases/legacyId/#{object}"
         rescue
-          throw :error_404
+          next
         end
         game_data = JSON.parse(object_response.body)
         game_data['data'].each do |game_data|
-          if game_data['metadata']['region'] == 'US'
             begin
               game_data['legacyData']['previewUrl']
             rescue
+              wordpress_preview_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
               puts object
               next
             end
             if (game_data['legacyData']['previewUrl'].to_s.match(/com\/articles\//) || game_data['legacyData']['previewUrl'].to_s.match(/blogs/) || game_data['legacyData']['previewUrl'] == nil || game_data['legacyData']['previewUrl'].to_s.length < 1)
-              #puts "PASS: http://apis.lan.ign.com/object/v3/releases/legacyId/#{object}"
             else
+              wordpress_preview_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
               puts object
-            end
           end
         end
       end
-    end
 
     %w(episodes shows volumes).each do |o|
-    catch (:error_404) do
       objectRelations.each do |object|
         begin
           object_response = RestClient.get "http://apis.lan.ign.com/object/v3/#{o}/legacyId/#{object}"
         rescue
-          throw :error_404
+          next
         end
         game_data = JSON.parse(object_response.body)
         begin
           game_data['legacyData']['previewUrl']
         rescue
+          wordpress_preview_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
           puts object
           next
         end
         if (game_data['legacyData']['previewUrl'].to_s.match(/com\/articles\//) || game_data['legacyData']['previewUrl'].to_s.match(/blogs/) || game_data['legacyData']['previewUrl'] == nil || game_data['legacyData']['previewUrl'].to_s.length < 1)
         else
+          wordpress_preview_urls << "http://write.ign.com/wp-admin/post.php?post=#{article['refs']['wordpressId']}&action=edit&message=1"
           puts object
         end
       end
     end
-    end
   end
-
-rescue => e
-  puts e.class.to_s
-  puts e.message.to_s
-  puts e.backtrace
-ensure
-  db.disconnect if db
-end
+  puts wordpress_preview_urls
