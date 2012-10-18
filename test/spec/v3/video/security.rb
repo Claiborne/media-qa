624b278201790136364ea442bdc19288be5b1934
Will Claiborne <wclaiborne@ign.com>
2012-10-18 13:41:03 -0700

    finish this weeks work for video api test automation for auth changes

--- test/spec/v3/video/security.rb
+++ test/spec/v3/video/security.rb
@@ -20,14 +20,19 @@ include TopazToken
 # GET PLAYLIST SEARCH USING STATE
 # GET PLAYLIST USING ?METADATA.STATE=STATE
 
-# SINGLE ENDPOINTS
-  #
+# GET SINGLE VIDEO USING ID AND SLUG
+  # CREATE
+  # GET VIDEO BY ID WITH OAUTH
+  # GET VIDEO BY SLUG  WITH OAUTH
+  # GET VIDEO BY ID WITHOUT OAUTH
+  # GET VIDEO BY SLUG  WITHOUT OAUTH
+
 
 ##################################################################
 # GET VIDEO USING STATE/STATE
 
 %w(discovered copying draft publishing publish_in_future failed).each do |state|
-describe "V3 Video API -- GET Ask For 'state/#{state}' Videos", :test => true do
+describe "V3 Video API -- GET Ask For 'state/#{state}' Videos" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -59,7 +64,7 @@ end end
 # GET VIDEO USING ?METADATA.STATE=STATE W/0 OAUTH
 
 %w(discovered copying draft publishing publish_in_future failed).each do |state|
-describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Videos", :test => true do
+describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Videos" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -88,10 +93,10 @@ describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Videos", :test 
 end end
 
 ##################################################################
-# POST VIDEO SEARCH USING STATE W/0 OAUTH
+# POST VIDEO SEARCH USING STATE
 
 %w(discovered copying draft publishing publish_in_future failed).each do |state|
-describe "V3 Video API -- POST Search Ask For #{state} Videos", :test => true do
+describe "V3 Video API -- POST Search Ask For #{state} Videos" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -120,10 +125,10 @@ describe "V3 Video API -- POST Search Ask For #{state} Videos", :test => true do
 end end
 
 ##################################################################
-# GET VIDEO SEARCH USING STATE W/0 OAUTH
+# GET VIDEO SEARCH USING STATE
 
 %w(discovered copying draft publishing publish_in_future failed).each do |state|
-describe "V3 Video API -- GET Search Ask For #{state} Videos", :test => true do
+describe "V3 Video API -- GET Search Ask For #{state} Videos" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -153,10 +158,10 @@ describe "V3 Video API -- GET Search Ask For #{state} Videos", :test => true do
 end end
 
 ##################################################################
-# GET PLAYLIST USING ?METADATA.STATE=STATE W/0 OAUTH
+# GET PLAYLIST USING ?METADATA.STATE=STATE
 
 %w(draft).each do |state|
-describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists", :test => true do
+describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -185,10 +190,10 @@ describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists", :te
 end end
 
 ##################################################################
-# POST PLAYLIST SEARCH USING STATE W/0 OAUTH
+# POST PLAYLIST SEARCH USING STATE
 
 %w(draft).each do |state|
-describe "V3 Video API -- POST Search Ask For #{state} Playlists", :test => true do
+describe "V3 Video API -- POST Search Ask For #{state} Playlists" do
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
     @config = Configuration.new
@@ -216,10 +221,10 @@ describe "V3 Video API -- POST Search Ask For #{state} Playlists", :test => true
 end end
 
 ##################################################################
-# GET PLAYLIST SEARCH USING STATE W/0 OAUTH
+# GET PLAYLIST SEARCH USING STATE
 
 %w(draft).each do |state|
-describe "V3 Video API -- GET Search Ask For #{state} Playlists", :test => true do
+describe "V3 Video API -- GET Search Ask For #{state} Playlists" do
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
     @config = Configuration.new
@@ -248,10 +253,10 @@ describe "V3 Video API -- GET Search Ask For #{state} Playlists", :test => true 
 end end
 
 ##################################################################
-# GET PLAYLIST USING ?METADATA.STATE=STATE W/0 OAUTH
+# GET PLAYLIST USING ?METADATA.STATE=STATE
 
 %w(draft).each do |state|
-describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists", :test => true do
+describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists" do
 
   before(:all) do
     Configuration.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
@@ -280,13 +285,27 @@ describe "V3 Video API -- GET Ask For '?metadata.state=#{state}' Playlists", :te
 end end
 
 ##################################################################
-# GET VIDEO USING STATE/STATE W/O OAUTH
+# GET SINGLE VIDEO USING ID AND SLUG
 
-describe "V3 Object API -- GET Specific Draft & Deleted Object", :stg1 => true do
+describe "V3 Video API -- GET Specific Non-Published Video", :stg => true do
 
   before(:all) do
-    TopazToken.set_token('objects')
+    TopazToken.set_token('videos')
     @base_url = "http://staging-videoapi.ign.com/v3/videos/"
+
+    @rand_num = Random.rand(500)
+    @vid_body = {"metadata"=>{"name"=>"media qa test #{@rand_num}","slug"=>"media-qa-test-#{@rand_num}","networks"=>["abc"]},"state"=>"discovered"}.to_json
+
+    class Vid
+      @video_id = "No ID"
+      def self.video_id=(value)
+        @video_id = value
+      end
+      def self.video_id
+        @video_id
+      end
+    end
+
   end
 
   before(:each) do
@@ -295,6 +314,130 @@ describe "V3 Object API -- GET Specific Draft & Deleted Object", :stg1 => true d
 
   after(:each) do
 
+  end
+
+  it "should create a video" do
+    res = RestClient.post "#{@base_url}?oauth_token=#{TopazToken.return_token}", @vid_body, :content_type => "application/json"
+    data = JSON.parse(res.body)
+    puts data['videoId']
+    Vid.video_id = @video_id = data['videoId']
+    res.code.should == 201
+  end
+
+  %w(discovered copying draft publishing publish_in_future abc failed).each do |state|
+
+    it "should change the video to state=#{state}" do
+      body = {"metadata"=>{"state"=>"#{state}"}}.to_json
+      res = RestClient.put "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}", body, :content_type => "application/json"
+      res.code.should == 200
+    end
+
+    it "should return a 200 when trying to GET a #{state} video by videoId with OAuth" do
+      res = RestClient.get "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}&fresh=true"
+      res.code.should == 200
+      d = JSON.parse(res.body)
+      d['metadata']['state'].should == state
+    end
+
+    it "should return a 200 when trying to GET a #{state} video by slug with OAuth" do
+      res = RestClient.get "#{@base_url}/slug/media-qa-test-#@rand_num?oauth_token=#{TopazToken.return_token}&fresh=true"
+      res.code.should == 200
+      d = JSON.parse(res.body)
+      d['metadata']['state'].should == state
+    end
+
+    it "should return a 401 when trying to GET a #{state} video by videoId without OAuth" do
+      expect {RestClient.get "#{@base_url}#{Vid.video_id}?fresh=true"}.to raise_error(RestClient::Unauthorized)
+    end
+
+    it "should return a 401 when trying to GET a #{state} video by slug without OAuth" do
+      expect {RestClient.get "#{@base_url}slug/media-qa-test-#@rand_num?fresh=true"}.to raise_error(RestClient::Unauthorized)
+    end
+
+  end
+
+  it "should clean up" do
+    res = RestClient.delete "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}"
+    res.code.should == 200
+  end
+
+end
+
+##################################################################
+# GET SINGLE PLAYLIST USING ID AND SLUG
+
+describe "V3 Video API -- GET Specific Non-Published Playlist", :stg => true do
+
+  before(:all) do
+    TopazToken.set_token('videos')
+    @base_url = "http://staging-videoapi.ign.com/v3/playlists/"
+
+    @rand_num = Random.rand(500)
+    @vid_body = {"metadata"=>{"name"=>"media qa test #{@rand_num}","slug"=>"media-qa-test-#{@rand_num}","networks"=>["abc"]},"state"=>"discovered"}.to_json
+
+    class Vid
+      @video_id = "No ID"
+      def self.video_id=(value)
+        @video_id = value
+      end
+      def self.video_id
+        @video_id
+      end
+    end
+
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should create a playlists" do
+    res = RestClient.post "#{@base_url}?oauth_token=#{TopazToken.return_token}", @vid_body, :content_type => "application/json"
+    data = JSON.parse(res.body)
+    puts data['playlistId']
+    Vid.video_id = @video_id = data['playlistId']
+    res.code.should == 201
+  end
+
+  %w(discovered copying draft publishing publish_in_future abc failed).each do |state|
+
+    it "should change the playlist to state=#{state}" do
+      body = {"metadata"=>{"state"=>"#{state}"}}.to_json
+      res = RestClient.put "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}", body, :content_type => "application/json"
+      res.code.should == 200
+    end
+
+    it "should return a 200 when trying to GET a #{state} playlist by playlistId with OAuth" do
+      res = RestClient.get "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}&fresh=true"
+      res.code.should == 200
+      d = JSON.parse(res.body)
+      d['metadata']['state'].should == state
+    end
+
+    it "should return a 200 when trying to GET a #{state} playlist by slug with OAuth" do
+      res = RestClient.get "#{@base_url}/slug/media-qa-test-#@rand_num?oauth_token=#{TopazToken.return_token}&fresh=true"
+      res.code.should == 200
+      d = JSON.parse(res.body)
+      d['metadata']['state'].should == state
+    end
+
+    it "should return a 401 when trying to GET a #{state} playlist by playlistId without OAuth" do
+      expect {RestClient.get "#{@base_url}#{Vid.video_id}?fresh=true"}.to raise_error(RestClient::Unauthorized)
+    end
+
+    it "should return a 401 when trying to GET a #{state} playlist by slug without OAuth" do
+      expect {RestClient.get "#{@base_url}slug/media-qa-test-#@rand_num?fresh=true"}.to raise_error(RestClient::Unauthorized)
+    end
+
+  end
+
+  it "should clean up" do
+    res = RestClient.delete "#{@base_url}#{Vid.video_id}?oauth_token=#{TopazToken.return_token}"
+    res.code.should == 200
   end
 
 end
