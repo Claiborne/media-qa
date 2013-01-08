d32500286a1e6c040e860b35fafb546aadf6e64d
Will Claiborne <wclaiborne@ign.com>
2013-01-08 13:43:50 -0800

    slight tweak to redirect checker

--- test/spec/scripts/check_redirects_in_api.rb
+++ test/spec/scripts/check_redirects_in_api.rb
@@ -20,33 +20,43 @@ file.close
 describe 'Test Redirects in Redirect API' do
 
   it 'should redirect' do
+    errors = []
+    fails = []
     list.each_with_index do |l, index|
-      sleep 0.07 # sleep 0.08 ~ 500 RPM; 0.04 ~ 750
+      sleep 0.06 # sleep 0.08 ~ 500 RPM; 0.04 ~ 750
       if index.even?
-        data = JSON.parse (RestClient.get "http://apis.lan.ign.com/redirect/v3/redirects?from=#{l}").body
+        begin
+          data = JSON.parse (RestClient.get "http://apis.lan.ign.com/redirect/v3/redirects?from=#{l}").body
+        rescue
+          puts ">>>"+l.to_s
+          next
+        end
         begin
           data[0]['from']
         rescue => e
-          puts ""
           puts l
-          puts 'Does not exist in redirect API:'
-          puts "Data: #{data}"
+          fails << l
+          errors <<  "FAILED :"+l.to_s
+          errors << 'Does not exist in redirect API:'
+          errors << "Data: #{data}"
           next
         end
         begin
           data[0]['from'].should == l
           list[index+1].match(data[0]['to']).should be_true
         rescue => e
-          next if list[index+1] == data[0]['to']+"ddd"
-          puts ""
-          puts e.message
-          puts l.to_s
-          puts "Expected: "+list[index+1].to_s+"|END"
-          puts "Got :     "+data[0]['to'].to_s+"|END"
+          next if list[index+1] == data[0]['to']
+          puts l
+          fails << l
+          errors <<  "FAILED :"+l.to_s
+          errors << e.message
+          errors << "Expected: "+list[index+1].to_s+"|END"
+          errors << "Got :     "+data[0]['to'].to_s+"|END"
         end
       end
       puts "CHECK # #{index/2}" if index % 2000 == 1 # Puts the index every 1000 checks
     end
+    File.open("/Users/wclaiborne/Desktop/errors.txt", 'w') { |f| f.write(errors) }
+    File.open("/Users/wclaiborne/Desktop/fails.txt", 'w') { |f| f.write(fails) }
   end
-
 end
