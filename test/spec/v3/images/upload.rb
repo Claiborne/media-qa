6bf3d893600b30611bed11bbcb926ac55864a95e
Will Claiborne <wclaiborne@ign.com>
2013-01-10 13:46:50 -0800

    progress with uploading image to image api

---
+++ test/spec/v3/images/upload.rb
@@ -0,0 +1,39 @@
+require 'rspec'
+require 'pathconfig'
+require 'rest_client'
+require 'json'
+require 'assert'
+require 'topaz_token'
+
+include TopazToken
+include Assert
+
+################################################################################
+
+describe "V3 Image API -- Should Upload An Image", :upload => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_images.yml"
+    @config = PathConfig.new
+    TopazToken.set_token('images')
+    @url = "http://media-image-stg-services-01.sfdev.colo.ignops.com:8080/image/upload?oauth_token=#{TopazToken.return_token}"
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it 'should' do
+
+    contents = File.open("/Users/wclaiborne/Desktop/rails.png", 'rb').read
+
+    File.open("/Users/wclaiborne/Desktop/x.jpg", 'wb') { |file|
+      file << contents
+    }
+  end
+end
