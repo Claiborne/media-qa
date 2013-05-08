5b6108fcd16bb3cedff74044b96da65828b58b1d
Will Claiborne <wclaiborne@ign.com>
2013-05-08 15:42:53 -0700

    modify perf scripts

--- test/performance/board_api.rb
+++ test/performance/board_api.rb
@@ -4,6 +4,5 @@ require 'rest_client'
 while true
   Thread.new do
     RestClient.get "http://10.97.64.101:8082/board/v3/boards"
-    RestClient.get "http://10.97.64.101:8082/board/v3/boards/512d2a63081652370062dec4"
   end
 end
--- test/performance/redirect_api.rb
+++ test/performance/redirect_api.rb
@@ -4,6 +4,5 @@ require 'rest_client'
 while true
   Thread.new do
     RestClient.get "http://10.97.64.101:8080/redirect/v3/redirects?from=http://xbox360.ign.com"
-    RestClient.get "http://10.97.64.101:8080/redirect/v3/redirects?from=http://pc.ign.com"
   end
 end
