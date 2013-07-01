5475adf116eb8dba7b1d318d8843a4d4aed6f242
Will Claiborne <wclaiborne@ign.com>
2013-07-01 16:59:17 -0700

    Repeating fields in the video API, such as tags.slug, don't work with a simple is or isNot comparison. The contains conditions allow you to compare a list of inputs to a repeating field. Added test automation for these use cases

--- test/spec/v3/video/general_get_search.rb
+++ test/spec/v3/video/general_get_search.rb
@@ -148,6 +148,140 @@ class VideoGetSearch
       "fromDate"=>"2011-01-01T00:00:00-0000",
       "toDate"=>"2011-12-31T00:00:00-0000"}.to_json
   end
+  
+  def self.contains_all
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"tags.slug",
+           "condition"=>"containsAll",
+           "value"=>"ps3,xbox-360"
+        },
+      ],
+      "startIndex"=>0,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.contains_none
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"tags.slug",
+           "condition"=>"containsNone",
+           "value"=>"ps3,xbox-360"
+        },
+      ],
+      "startIndex"=>0,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.contains_one(n)
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"tags.slug",
+           "condition"=>"containsOne",
+           "value"=>"ps3,xbox-360"
+        },
+      ],
+      "startIndex"=>n.to_i,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.object_relations_contains(n)
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"objectRelations.platform",
+           "condition"=>"containsOne",
+           "value"=>"PlayStation 3,PC"
+        },
+        {
+           "field"=>"objectRelations.platform",
+           "condition"=>"containsNone",
+           "value"=>"Xbox 360"
+        },
+      ],
+      "startIndex"=>n.to_i,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.tag_contains
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"tags.slug",
+           "condition"=>"containsAll",
+           "value"=>"xbox-360,review"
+        },
+        {
+           "field"=>"tags.slug",
+           "condition"=>"containsNone",
+           "value"=>"ps3,movies,comics,ds"
+        },
+      ],
+      "startIndex"=>0,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.category_contains
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"category",
+           "condition"=>"containsAll",
+           "value"=>"ign_all,xbox-360_all"
+        },
+        {
+           "field"=>"category",
+           "condition"=>"containsNone",
+           "value"=>"ps3_all,ds_all,pc_all"
+        },
+      ],
+      "startIndex"=>0,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
+  
+  def self.category_contains_one
+    {
+      "matchRule"=>"matchAll",
+      "rules"=>[
+        {
+           "field"=>"category",
+           "condition"=>"containsOne",
+           "value"=>"ds_all"
+        },
+      ],
+      "startIndex"=>0,
+      "count"=>200,
+      "states"=>"published",
+      "networks"=>"ign"
+    }.to_json
+  end
 
 end
 
@@ -208,11 +342,9 @@ describe "V3 Video API -- GET Search Using #{VideoGetSearch.is_classification(co
     end
   end
 
-  it "should return only videos with a networl value of 'ign'" do
-    @data['data'].each do |k|
-      k['metadata']['networks'].each do |network|
-        network.include?("ign").should be_true
-      end
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
     end
   end
 
@@ -540,6 +672,493 @@ describe "V3 Video API -- GET Search Using #{VideoGetSearch.video_series}" do
     end
   end
 
+end
+
+################################################################################
+
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.contains_all}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.contains_all
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with tags 'ps3' and 'xbox-360' " do
+    @data['data'].each do |d|
+      tags = []
+      d['tags'].each do |t|
+        tags << t['slug']
+      end
+      tags.include?('ps3').should be_true
+      tags.include?('xbox-360').should be_true
+    end
+  end
+  
+end
+
+################################################################################
+
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.contains_none}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.contains_none
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only videos without tags 'ps3' or 'xbox-360' " do
+    @data['data'].each do |d|
+      tags = []
+      d['tags'].each do |t|
+        tags << t['slug']
+      end
+      tags.include?('ps3').should be_false
+      tags.include?('xbox-360').should be_false
+    end
+  end
+  
+end
+
+################################################################################
+
+%w(0 200 400 600).each do |num|
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.contains_one(num)}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.contains_one(num)
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+
+  it "should return only videos with tags 'ps3' or 'xbox-360' " do
+    @data['data'].each do |d|
+      tags = []
+      d['tags'].each do |t|
+        tags << t['slug']
+      end
+      (tags.include?('ps3') ||  tags.include?('xbox-360')).should be_true
+    
+    end
+  end
+  
+end
+end
+
+################################################################################
+
+%w(0 200 400 600).each do |num|
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.object_relations_contains(num)}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.object_relations_contains(num)
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only videos with objectRelation.platforms values of 'PlayStation 3' or 'PC'" do
+    @data['data'].each do |d|
+      platforms = []
+      d['objectRelations'].each do |o|
+        platforms << o['platform']
+      end
+      (platforms.include?('PlayStation 3') || platforms.include?('PC')).should be_true
+    end
+  end
+  
+  it "should not return  videos with objectRelation.platforms values of 'Xbox 360'" do
+    @data['data'].each do |d|
+      platforms = []
+      d['objectRelations'].each do |o|
+        platforms << o['platform']
+      end
+      platforms.include?('Xbox 360').should be_false
+    end
+  end
+end
+end
+
+################################################################################
+
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.tag_contains}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.tag_contains
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only videos with tags.slug values of 'xbox-360' and 'review'" do
+    @data['data'].each do |d|
+      tags = []
+      d['tags'].each do |t|
+        tags << t['slug']
+      end
+      tags.include?('xbox-360').should be_true
+      tags.include?('review').should be_true
+    end
+  end
+  
+  it "should not return videos with tags.slug values of 'ps3', 'movies', 'comics', 'ds'" do
+    @data['data'].each do |d|
+      tags = []
+      d['tags'].each do |t|
+        tags << t['slug']
+      end
+      tags.include?('ps3').should be_false
+      tags.include?('movies').should be_false
+      tags.include?('comics').should be_false
+      tags.include?('ds').should be_false
+    end
+  end
+  
+end
+
+################################################################################
+
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.category_contains}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.category_contains
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return at least 90 videos" do
+    @data['data'].count.should > 89
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only videos with a category of 'ign_all' and 'xbox-360_all' " do
+    @data['data'].each do |d|
+      d['category'].include?('ign_all').should be_true
+      d['category'].include?('xbox-360_all').should be_true
+    end
+  end
+  
+  it "should not return videos with a category of 'ps3_all', 'ds_all', or 'pc_all' " do
+    @data['data'].each do |d|
+      d['category'].include?('ps3_all').should be_false
+      d['category'].include?('ds_all').should be_false
+      d['category'].include?('pc_all').should be_false
+    end
+  end
+  
+end
+
+################################################################################
+
+describe "V3 Video API -- GET Search Using #{VideoGetSearch.category_contains_one}", :test => true do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_video.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/v3/videos/search?q="+VideoGetSearch.category_contains_one
+    @url = @url.to_s.gsub(/\"|\{|\}|\||\\|\^|\[|\]|`|\s+/) { |m| CGI::escape(m) }
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse(@response.body)
+  end
+
+  before(:each) do
+
+  end
+
+  after(:each) do
+
+  end
+
+  it "should return 200" do
+    check_200(@response)
+  end
+
+  it "should not be blank" do
+    check_not_blank(@data)
+  end
+
+  it "should return a hash with six indices" do
+    check_indices(@data, 6)
+  end
+  
+  it "should return 200 videos" do
+    @data['data'].count.should == 200
+  end
+  
+  it "should return only published videos" do
+    @data['data'].each do |d|
+      d['metadata']['state'].should == "published"
+    end
+  end
+  
+  it "should return only videos with a network value of 'ign'" do
+    @data['data'].each do |d|
+      d['metadata']['networks'].include?('ign').should be_true
+    end
+  end
+  
+  it "should return only videos with a category of 'ds_all'" do
+    @data['data'].each do |d|
+      d['category'].include?('ds_all').should be_true
+    end
+  end
+  
+  
 end
 
 # PLAYLIST SEARCH TESTS
