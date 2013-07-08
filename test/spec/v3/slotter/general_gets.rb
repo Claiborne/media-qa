86a56f29f8190faaf4741badd985e1590ba7b89e
Will Claiborne <wclaiborne@ign.com>
2013-07-08 14:18:32 -0700

    more test cases for slotter api GETs

--- test/spec/v3/slotter/general_gets.rb
+++ test/spec/v3/slotter/general_gets.rb
@@ -4,6 +4,7 @@ require 'pathconfig'
 require 'rest_client'
 require 'json'
 require 'assert'
+require 'time'
 
 include Assert
 
@@ -12,7 +13,7 @@ describe "V3 Slotter API -- GET /slotters" do
   before(:all) do
     PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_slotter.yml"
     @config = PathConfig.new
-    @url = "http://#{@config.options['baseurl']}/slotters"
+    @url = "http://#{@config.options['baseurl']}/slotters?fresh=true"
     begin
       @response = RestClient.get @url
     rescue => e
@@ -22,7 +23,6 @@ describe "V3 Slotter API -- GET /slotters" do
   end
 
   it "should return a 200 response code" do
-    puts @url
     check_200(@response)
   end
   
@@ -30,4 +30,58 @@ describe "V3 Slotter API -- GET /slotters" do
     @data.length.should == 100
   end
   
+  %w(id timestamp principal name slug _rTimestamp).each do |key|
+  it "should return a non-nil, non-blank #{key} value for all entries" do
+    @data.each do |d|
+      begin
+        check_not_blank d[key]
+        check_not_nil d[key]
+      rescue => e
+        raise e, e.message+". Non-nil or blank #{key} value here: \n #{d}"
+      end
+    end 
+  end
+  end
+
+end
+%w(id slug).each do |field|
+describe "V3 Slotter API -- GET by /#{field.upcase}" do
+
+  before(:all) do
+    PathConfig.config_path = File.dirname(__FILE__) + "/../../../config/v3_slotter.yml"
+    @config = PathConfig.new
+    @url = "http://#{@config.options['baseurl']}/slotters?fresh=true"
+    begin
+      @response = RestClient.get @url
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end
+    @data = JSON.parse @response.body
+    begin
+      @slotted = @data[0]
+    rescue => e
+      raise e, "Unable to set up this test case"
+    end
+    begin
+      @response_id = RestClient.get "http://#{@config.options['baseurl']}/slotters/#{@slotted[field]}?fresh=true"
+    rescue => e
+      raise Exception.new(e.message+" "+@url)
+    end    
+    @data_id = JSON.parse @response_id.body
+  end
+  
+  it "should return a 200 response code" do
+    check_200(@response)
+  end
+  
+  it "should return the same data wehn seen in /slotters list" do
+    begin
+      @data_id.should == @slotted
+    rescue => e
+      time_diff = Time.parse(@data_id['_rTimestamp']) - Time.parse(@slotted['_rTimestamp'])
+      raise e, e.message unless time_diff.abs < 10
+    end
+  end
+  
+end
 end
