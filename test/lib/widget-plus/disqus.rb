40fd1618d6bf33b4576733934b0ea210096f1855
Will Claiborne <wclaiborne@ign.com>
2013-07-16 17:20:42 -0700

    making assertins on the disqus widget now only checks for > 2 iframes instead of == 4

--- test/lib/widget-plus/disqus.rb
+++ test/lib/widget-plus/disqus.rb
@@ -4,7 +4,7 @@ module Disqus
 
     it "should display once" do
       @selenium.find_elements(:css => "div#disqus_thread").count.should == 1
-      @selenium.find_elements(:css => "div#disqus_thread iframe").count.should == 4
+      @selenium.find_elements(:css => "div#disqus_thread iframe").count.should > 2
       @selenium.find_element(:css => "div#disqus_thread iframe#dsq3").displayed?.should be_true
     end
 
