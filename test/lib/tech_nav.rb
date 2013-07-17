99b9045efc6767ce89d01a5a852534254b0f4409
Will Claiborne <wclaiborne@ign.com>
2013-07-17 11:16:56 -0700

    update tech nav list

--- test/lib/tech_nav.rb
+++ test/lib/tech_nav.rb
@@ -1,5 +1,5 @@
 module TechNav
   def return_tech_nav
-    ["nintendo-3ds", "android", "home-theater", "ipad", "ipod", "iphone", "lifestyle", "mac", "wii-u", "pc", "ps-vita", "playstation-3", "xbox-360", "windows-phone", "playstation-4"]
+    ["nintendo-3ds", "android", "home-theater", "ipad", "ipod", "iphone", "lifestyle", "mac", "wii-u", "pc", "ps-vita", "playstation-3" ,"playstation-4", "xbox-360", "xbox-one", "windows-phone"]
   end
 end
