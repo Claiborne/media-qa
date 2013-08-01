f19adbe941857e419ef5b540888e33ed8d9708d0
Will Claiborne <wclaiborne@ign.com>
2013-08-01 10:56:26 -0700

    initial commit for hack day 2013 (rails app) project

---
+++ hackday/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackday::Application.config.secret_token = '97ec9fe87f54683d7ece498539c17f5f454817702f80cedfacf32a66c3a69cfdaabfb582ca57bfd962a5c3b7ed0c3708a13502106dfadbc30a5e6708f3640275'
