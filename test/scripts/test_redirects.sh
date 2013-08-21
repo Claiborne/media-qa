6609bd279f4b0fdcdb9fbeec5ed47fb01d041ca9
Will Claiborne <wclaiborne@ign.com>
2013-08-21 15:11:16 -0700

    adding scripts to test new redirect farm

---
+++ test/scripts/test_redirects.sh
@@ -0,0 +1,1 @@
+curl --verbose --header "Host: $1" "http://ign-redirect-prd-web-01.aws1.ignops.com/$2"
---
+++ test/scripts/test_redirects2.sh
@@ -0,0 +1,2 @@
+curl --verbose --header "Host: $1" "http://ign-redirect-prd-web-02.aws1.ignops.com/$2"
+
